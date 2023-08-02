d71f7932699bc76bd25db8f3481aa856a53ef6ec
HarryM3103 <hemoore3@buffalo.edu>
2023-08-02 12:11:20 -0400

    Website Project

---
+++ main.py
@@ -0,0 +1,142 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+from flask import Flask, render_template
+from store.parsing import gpuCSV_parser, sort_best_value
+from store.gpuItem import GpuItem
+
+app = Flask(__name__)
+
+url = "https://www.newegg.com/p/pl?d=graphics+cards"
+
+headers = [
+    "Image Link",
+    "Brand",
+    "Item Link",
+    "Item Name",
+    "Current Price",
+    "Previous Price",
+    "Savings",
+    "Shipping Price",
+    "Item rating",
+    "Number of ratings",
+]
+
+# Create csv file for information
+
+
+@app.route("/data")
+def data_collector():
+    with open("GPU.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+    for pages in range(1, 15):
+        site = requests.get(url + "&pages=" + str(pages))
+        page = BeautifulSoup(site.content, "html.parser")
+
+        containers = page.find_all("div", {"class": "item-container"})
+
+        for container in containers:
+            # Item Image
+            item_img = container.img["src"]
+            # Item brand
+            try:
+                brand_info = container.find("div", {"class": "item-branding"}).a.img[
+                    "title"
+                ]
+            except:
+                brand_info = None
+
+            # Item link
+            try:
+                item_link = container.a["href"]
+            except:
+                item_link = None
+
+            # Item title
+            try:
+                item_title = container.find("a", {"class": "item-title"}).text
+            except:
+                item_title = None
+
+            # Item price (current)
+            try:
+                item_price = (
+                    "$" + container.find("li", {"class": "price-current"}).strong.text
+                )
+            except:
+                item_price = None
+
+            # Item's previous price
+            try:
+                item_previous_price = container.find(
+                    "span", {"class": "price-was-data"}
+                ).text
+            except:
+                item_previous_price = None
+
+            # Item's savings
+            try:
+                item_save = container.find("span", {"class": "price-save-percent"}).text
+            except:
+                item_save = None
+
+            # Item rating
+            try:
+                item_rating = container.find("i", {"class": "rating rating-5"})[
+                    "aria-label"
+                ]
+
+            except:
+                try:
+                    item_rating = container.find("i", {"class": "rating rating-4-5"})[
+                        "aria-label"
+                    ]
+                except:
+                    try:
+                        item_rating = container.find("i", {"class": "rating rating-4"})[
+                            "aria-label"
+                        ]
+                    except:
+                        item_rating = None
+
+            try:
+                num_ratings_raw = container.find(
+                    "span", {"class": "item-rating-num"}
+                ).text
+                num_ratings = num_ratings_raw.split("(")[1].split(")")[0]
+            except:
+                num_ratings = None
+
+            # Item shipping price
+            try:
+                shipping = container.find("li", {"class": "price-ship"}).text
+            except:
+                shipping = None
+
+            # Group data into list to be added csv file
+            data = [
+                item_img,
+                brand_info,
+                item_link,
+                item_title,
+                item_price,
+                item_previous_price,
+                item_save,
+                shipping,
+                item_rating,
+                num_ratings,
+            ]
+
+            # Add the parsed data to the csv file
+            with open("GPU.csv", "a", newline="") as g:
+                writer = csv.writer(g)
+                writer.writerow(data)
+
+    return "GPU.csv"
+
+
+sort = sort_best_value()
+for i in sort:
+    print(f"{i.bayasian_avg}: {i.brand}: {i.name}")
---
+++ store/parsing.py
@@ -0,0 +1,55 @@
+import csv
+from store.gpuItem import GpuItem
+
+
+def gpuCSV_parser():
+    gpu_List: list[GpuItem] = []
+    with open("GPU.csv", "r") as f:
+        counter = 0
+        reader = csv.reader(f)
+        next(reader, None)
+        for line in reader:
+            if counter != 1:
+                counter += 1
+                continue
+            gpu = GpuItem()
+            gpu.image = line[0]
+            gpu.brand = line[1]
+            gpu.link = line[2]
+            gpu.name = line[3]
+            if line[4] != "":
+                try:
+                    gpu.current_price = float(line[4].split("$")[1])
+                except:
+                    gpu.current_price = float(line[4].split("$")[1].replace(",", ""))
+            if line[5] != "":
+                try:
+                    gpu.current_price = float(line[5].split("$")[1])
+                except:
+                    gpu.current_price = float(line[5].split("$")[1].replace(",", ""))
+            if line[6] != "":
+                gpu.savings = int(line[6].split("%")[0])
+            if line[7] != "":
+                gpu.shipping = line[7]
+            if line[8] != "":
+                gpu.item_rating = float(line[8].split()[1])
+            if line[9] != "":
+                try:
+                    gpu.ratings_num = int(line[9])
+                except:
+                    gpu.ratings_num = int(line[9].replace(",", ""))
+            if any(x.name == line[3] for x in gpu_List):
+                continue
+            else:
+                gpu_List.append(gpu)
+    return gpu_List
+
+
+def sort_best_value():
+    item_list = gpuCSV_parser()
+    bayasian_list: GpuItem = []
+    for item in item_list:
+        item.bayasian_calc()
+        bayasian_list.append(item)
+    sorted_bayasian = sorted(bayasian_list, key=lambda x: x.bayasian_avg, reverse=True)
+    return sorted_bayasian
