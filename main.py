947289afc70e8648ce2d20f7e9726f0b5539b8c9
HarryM3103 <hemoore3@buffalo.edu>
2023-08-18 02:57:58 -0400

    more front end work

--- main.py
+++ main.py
@@ -1,127 +1,12 @@
-from bs4 import BeautifulSoup
 import json
-import requests
 import csv
 from flask import Flask, render_template, request
-from store.parsing import gpuCSV_parser, sort_best_value
+from store.parsing import gpuCSV_parser, sort_best_value, data_collector
 from store.gpuItem import GpuItem
 
 app = Flask(__name__)
 
 SEARCH = ""
-
-
-def data_collector(search):
-    data = []
-    url = "https://www.newegg.com/p/pl?d=" + search
-
-    for pages in range(1, 15):
-        site = requests.get(url + "&pages=" + str(pages))
-        page = BeautifulSoup(site.content, "html.parser")
-
-        containers = page.find_all("div", {"class": "item-container"})
-
-        for container in containers:
-            # Item Image
-            item_img = container.img["src"]
-
-            # Item link
-            try:
-                item_link = container.a["href"]
-            except:
-                item_link = None
-
-            # Item title
-            try:
-                item_title = container.find("a", {"class": "item-title"}).text
-            except:
-                item_title = None
-
-            # Item brand
-            try:
-                brand_info = container.find("div", {"class": "item-branding"}).a.img[
-                    "title"
-                ]
-            except:
-                if (item_title.split()[0].lower() == "be") or (
-                    item_title.split()[0].lower() == "cooler"
-                ):
-                    brand_info = f"{item_title.split()[0]} {item_title.split()[1]}"
-                else:
-                    brand_info = item_title.split()[0]
-
-            # Item price (current)
-            try:
-                item_price = (
-                    "$" + container.find("li", {"class": "price-current"}).strong.text
-                )
-            except:
-                item_price = None
-
-            # Item's previous price
-            try:
-                item_previous_price = container.find(
-                    "span", {"class": "price-was-data"}
-                ).text
-            except:
-                item_previous_price = None
-
-            # Item's savings
-            try:
-                item_save = container.find("span", {"class": "price-save-percent"}).text
-            except:
-                item_save = None
-
-            # Item rating
-            try:
-                item_rating = container.find("i", {"class": "rating rating-5"})[
-                    "aria-label"
-                ]
-
-            except:
-                try:
-                    item_rating = container.find("i", {"class": "rating rating-4-5"})[
-                        "aria-label"
-                    ]
-                except:
-                    try:
-                        item_rating = container.find("i", {"class": "rating rating-4"})[
-                            "aria-label"
-                        ]
-                    except:
-                        item_rating = None
-
-            try:
-                num_ratings_raw = container.find(
-                    "span", {"class": "item-rating-num"}
-                ).text
-                num_ratings = num_ratings_raw.split("(")[1].split(")")[0]
-            except:
-                num_ratings = None
-
-            # Item shipping price
-            try:
-                shipping = container.find("li", {"class": "price-ship"}).text
-            except:
-                shipping = None
-
-            # Group data into list
-            data_entries = [
-                item_img,
-                brand_info,
-                item_link,
-                item_title,
-                item_price,
-                item_previous_price,
-                item_save,
-                shipping,
-                item_rating,
-                num_ratings,
-            ]
-            data.append(data_entries)
-            print(data_entries)
-
-    return data
 
 
 # Initialize the html file
--- store/parsing.py
+++ store/parsing.py
@@ -1,5 +1,121 @@
 import csv
+import requests
+from bs4 import BeautifulSoup
 from store.gpuItem import GpuItem
+
+
+def data_collector(search):
+    data = []
+    revised_search = search.replace(" ", "+")
+    print(revised_search)
+    url = "https://www.newegg.com/p/pl?d=" + revised_search
+
+    for pages in range(1, 15):
+        site = requests.get(url + "&pages=" + str(pages))
+        page = BeautifulSoup(site.content, "html.parser")
+
+        containers = page.find_all("div", {"class": "item-container"})
+
+        for container in containers:
+            # Item Image
+            item_img = container.img["src"]
+
+            # Item link
+            try:
+                item_link = container.a["href"]
+            except:
+                item_link = None
+
+            # Item title
+            try:
+                item_title = container.find("a", {"class": "item-title"}).text
+            except:
+                item_title = None
+
+            # Item brand
+            try:
+                brand_info = container.find("div", {"class": "item-branding"}).a.img[
+                    "title"
+                ]
+            except:
+                if (item_title.split()[0].lower() == "be") or (
+                    item_title.split()[0].lower() == "cooler"
+                ):
+                    brand_info = f"{item_title.split()[0]} {item_title.split()[1]}"
+                else:
+                    brand_info = item_title.split()[0]
+
+            # Item price (current)
+            try:
+                item_price = (
+                    "$" + container.find("li", {"class": "price-current"}).strong.text
+                )
+            except:
+                item_price = None
+
+            # Item's previous price
+            try:
+                item_previous_price = container.find(
+                    "span", {"class": "price-was-data"}
+                ).text
+            except:
+                item_previous_price = None
+
+            # Item's savings
+            try:
+                item_save = container.find("span", {"class": "price-save-percent"}).text
+            except:
+                item_save = None
+
+            # Item rating
+            try:
+                item_rating = container.find("i", {"class": "rating rating-5"})[
+                    "aria-label"
+                ]
+
+            except:
+                try:
+                    item_rating = container.find("i", {"class": "rating rating-4-5"})[
+                        "aria-label"
+                    ]
+                except:
+                    try:
+                        item_rating = container.find("i", {"class": "rating rating-4"})[
+                            "aria-label"
+                        ]
+                    except:
+                        item_rating = None
+
+            try:
+                num_ratings_raw = container.find(
+                    "span", {"class": "item-rating-num"}
+                ).text
+                num_ratings = num_ratings_raw.split("(")[1].split(")")[0]
+            except:
+                num_ratings = None
+
+            # Item shipping price
+            try:
+                shipping = container.find("li", {"class": "price-ship"}).text
+            except:
+                shipping = None
+
+            # Group data into list
+            data_entries = [
+                item_img,
+                brand_info,
+                item_link,
+                item_title,
+                item_price,
+                item_previous_price,
+                item_save,
+                shipping,
+                item_rating,
+                num_ratings,
+            ]
+            data.append(data_entries)
+
+    return data
 
 
 def gpuCSV_parser(data):
@@ -30,7 +146,10 @@ def gpuCSV_parser(data):
             try:
                 gpu.ratings_num = int(entries[9])
             except:
-                gpu.ratings_num = int(entries[9].replace(",", ""))
+                try:
+                    gpu.ratings_num = int(entries[9].replace(",", ""))
+                except:
+                    gpu.ratings_num = 0
         if any(x.name == entries[3] for x in gpu_List):
             continue
         else:
