4c201ef4092121f9d7848b53090cbccf6824c438
HarryM3103 <hemoore3@buffalo.edu>
2025-07-27 15:20:01 -0400

    Separated exception handling in data_collection() into another file

--- store/parsing.py
+++ store/parsing.py
@@ -2,6 +2,7 @@ import requests
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from multiprocessing import Queue
+from store.exception_handling import rawHTML_Handling
 from store.ProductItem import ProductItem
 
 # TODO Finish documenting parsing.py
@@ -65,107 +66,7 @@ def data_collector(search: str, page_num: int, data_store: Queue):
             if (container.find("i", {"class": "fas fa-info-circle-light"}) is not None):
                 continue
             else:
-                # Item Image
-                item_img = container.img["src"]
-
-                # Item link
-                try:
-                    item_link = container.a["href"]
-                except:
-                    item_link = None
-
-                # Item title
-                try:
-                    item_title = container.find(
-                        "a", {"class": "item-title"}).text
-                except:
-                    item_title = None
-
-                # Item brand
-                try:
-                    brand_info = container.find("div", {"class": "item-branding"}).a.img[
-                        "title"
-                    ]
-                except:
-                    if (item_title.split()[0].lower() == "be") or (
-                        item_title.split()[0].lower() == "cooler"
-                    ):
-                        brand_info = f"{item_title.split()[0]} {item_title.split()[1]}"
-                    else:
-                        brand_info = item_title.split()[0]
-
-                # Item price (current)
-                try:
-                    item_price = (
-                        "$" +
-                        container.find(
-                            "li", {"class": "price-current"}).strong.text
-                    )
-                except:
-                    item_price = None
-
-                # Item's previous price
-                try:
-                    item_previous_price = container.find(
-                        "span", {"class": "price-was-data"}
-                    ).text
-                except:
-                    item_previous_price = None
-
-                # Item's savings
-                try:
-                    item_save = container.find(
-                        "span", {"class": "price-save-percent"}).text
-                except:
-                    item_save = None
-
-                # Item rating
-                try:
-                    item_rating = container.find("i", {"class": "rating rating-5"})[
-                        "aria-label"
-                    ]
-
-                except:
-                    try:
-                        item_rating = container.find("i", {"class": "rating rating-4-5"})[
-                            "aria-label"
-                        ]
-                    except:
-                        try:
-                            item_rating = container.find("i", {"class": "rating rating-4"})[
-                                "aria-label"
-                            ]
-                        except:
-                            item_rating = None
-
-                try:
-                    num_ratings_raw = container.find(
-                        "span", {"class": "item-rating-num"}
-                    ).text
-                    num_ratings = num_ratings_raw.split("(")[1].split(")")[0]
-                except:
-                    num_ratings = None
-
-                # Item shipping price
-                try:
-                    shipping = container.find(
-                        "li", {"class": "price-ship"}).text
-                except:
-                    shipping = None
-
-                # Group data into list
-                data_entries = [
-                    item_img,
-                    brand_info,
-                    item_link,
-                    item_title,
-                    item_price,
-                    item_previous_price,
-                    item_save,
-                    shipping,
-                    item_rating,
-                    num_ratings,
-                ]
+                data_entries = rawHTML_Handling(container=container)
                 data.append(data_entries)
         data_store.put(data)
     else:
@@ -188,22 +89,22 @@ def item_parser(data_entry: list[str]) -> ProductItem:
     if current_price is not None:
         try:
             product.current_price = float(current_price.split("$")[1])
-        except:
+        except:  # noqa: E722
             try:
                 product.current_price = float(
                     current_price.split("$")[1].replace(",", ""))
-            except:
+            except:  # noqa: E722
                 return
     if previous_price is not None:
         try:
             product.previous_price = float(previous_price.split("$")[1])
-        except:
+        except:  # noqa: E722
             product.previous_price = float(
                 previous_price.split("$")[1].replace(",", ""))
     if savings is not None:
         try:
             product.savings = int(savings.split("%")[0])
-        except:
+        except:  # noqa: E722
             product.savings = 0
     if shipping is not None:
         product.shipping = shipping
@@ -212,10 +113,10 @@ def item_parser(data_entry: list[str]) -> ProductItem:
     if ratings is not None:
         try:
             product.ratings_num = int(ratings)
-        except:
+        except:  # noqa: E722
             try:
                 product.ratings_num = int(ratings.replace(",", ""))
-            except:
+            except:  # noqa: E722
                 product.ratings_num = 0
     return product
 
@@ -227,7 +128,7 @@ def sort_best_value(data: list[list[str]]) -> list[str]:
         try:
             item.bayasian_calc()
             bayasian_list.append(item)
-        except:
+        except:  # noqa: E722
             continue
     sorted_bayasian = sorted(
         bayasian_list,
